15882f77cd8f28bd5a285f9e6fb703f69d281202
Mathivannan Srinivasan <mathi@Mathivannans-MacBook-Air.local>
2022-12-11 21:13:29 -0500

    Added Remote Exception

--- src/paxos/IServer.java
+++ src/paxos/IServer.java
@@ -1,8 +1,9 @@
 package paxos;
 
 import java.rmi.Remote;
+import java.rmi.RemoteException;
 
 public interface IServer<T> extends Remote {
-  Acceptor<T> getAcceptor();
-  void applyChanges(T operation);
+  Acceptor<T> getAcceptor() throws RemoteException;;
+  void applyChanges(T operation) throws RemoteException;;
 }
